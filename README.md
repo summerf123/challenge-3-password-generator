e761b18e273cf5789588dc745276669b3ccbf369
Summer Farrell <summer.farrell@teledyne.com>
2022-10-11 14:47:07 -0400

    added while loop when user does not select any special characters

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ## App Description
 
-this app is designed to generate a random password for a user. It asks the user how long they want their password to be and what types of characters they would like to include in their password, such as special characters, uppercase, lowercase and numbers. The user has to select the length by inputting the value in the prompt, no less than 8 characters, and no more than 128, and include (say 'OK') at least one type of special character.
+this app is designed to generate a random password for a user. It asks the user how long they want their password to be and what types of characters they would like to include in their password, such as special characters, uppercase, lowercase and numbers. The user has to select the length by inputting the value in the prompt, no less than 8 characters, and no more than 128, and include at least one type of special character via the 'confirm' boxes.
 
 ![Alt text](/images/passwordGenPhoto.png "My Password Generator")
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Assignment Code
+// assign variable generateBtn and make it equal to return the first element with id of generate, whic is out button
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
 // passwordOptions contains all necessary string data needed to generate the password for user
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = [
@@ -76,37 +77,44 @@ var uppercase = [
 
 //create a function that will execute and run the code like a for loop
 function initializePassword() {
-  //ask user how many characters they want the password to be
+  //use a prompt to ask user how many characters they want the password to be
   let preferredLengthConfirm = prompt(
     "how many characters do you want password to be? choose at least 8 characters and no more than 128"
   );
 
   console.log("length want password to be: " + preferredLengthConfirm);
 
-  //need to run this sequence each time input is too short or too long but executing the function initializePassword all over again
-
-  if (preferredLengthConfirm < 8) {
+  //need to run this sequence each time input is too short or too long by executing the function initializePassword all over again if input does not satisfy criteria
+  if (!preferredLengthConfirm) {
+    alert("you cancelled the password generator");
+    return;
+  } else if (preferredLengthConfirm < 8) {
     alert("your input is too short");
     initializePassword();
   } else if (preferredLengthConfirm > 128) {
     alert("your password is too long!");
     initializePassword();
-  } else {
+  } else if (!/^[0-9]+$/.test(preferredLengthConfirm)) {
+    alert("You did not enter a number.Please enter the desired length");
+    initializePassword();
+  } else if (!preferredLengthConfirm) {
+    alert("you cancelled the password generator");
+    return;
+  } else
     alert(
       `okay, your password will be ${preferredLengthConfirm} characters long`
     );
-    generatePassword(preferredLengthConfirm);
-    //User input for the preferredLengthConfirm is the desired length. This value is called in the function below in the passwordLength parameter and is inserted there to run the function.
-  }
+  generatePassword(preferredLengthConfirm);
+  //User input for the preferredLengthConfirm is the desired length. This value is called in the function below in the passwordLength parameter and is inserted there to run the function.
 }
+
 function generatePassword(passwordLength) {
   //run confirm boxes to see what type of characters user wants to include in password
   let specialCharConfirm = confirm(
     "Click 'OK' if you want to include special characters?"
   );
-  {
-    console.log("want special characters? " + specialCharConfirm);
-  }
+
+  console.log("want special characters? " + specialCharConfirm);
 
   let uppercaseConfirm = confirm(
     "Click 'OK' if you want to include uppercase characters?"
@@ -120,8 +128,19 @@ function generatePassword(passwordLength) {
     console.log("want lowercase? " + lowercaseConfirm);
 
     let numConfirm = confirm('Click "OK" if you want to include numbers');
-    console.log("want numbers? " + lowercaseConfirm);
-    //create empty array called newArray that uses a spread operator.............
+    console.log("want numbers? " + numConfirm);
+    // Loop to beginning of generatePW function if user never selects any speical characters
+    // while (
+    //   specialCharConfirm === false &&
+    //   uppercaseConfirm === false &&
+    //   lowercaseConfirm === false &&
+    //   numConfirm === false
+    // ) {
+    //   alert("You must choose at least one special character parameter");
+    //   generatePassword();
+    // }
+
+    //create empty array called newArray that uses a spread operator to spread the array of each declared array at the top
     let newArray = [];
     if (uppercaseConfirm) {
       newArray = [...newArray, ...uppercase];
@@ -129,15 +148,15 @@ function generatePassword(passwordLength) {
     }
     if (lowercaseConfirm) {
       newArray = [...newArray, ...lowercase];
-      console.log(newArray);
+      // console.log(newArray);
     }
     if (specialCharConfirm) {
       newArray = [...newArray, ...specialChar];
-      console.log(newArray);
+      // console.log(newArray);
     }
     if (numConfirm) {
       newArray = [...newArray, ...numbers];
-      console.log(newArray);
+      // console.log(newArray);
     }
     // console.log(newArray);
 
@@ -148,17 +167,9 @@ function generatePassword(passwordLength) {
     }
     console.log(randomPassword);
     document.getElementById("password").innerHTML = randomPassword;
-
-    //   if (uppercaseConfirm === true) {
-    //     alert("OK your password will contain special characters");
-    //   } else if (uppercaseConfirm === false) {
-    //     alert("You do not want to include special characters");
-    //   }
-    //   //STORE the data to include/not include uppercase characters based on user input***********
   }
 }
 generateBtn.addEventListener("click", initializePassword);
-
 // ===========original code given===============
 // // Assignment Code
 // var generateBtn = document.querySelector("#generate");
